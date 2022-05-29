9de21ecf89bb004d169729878fe47dbb0faf3dd7
hayato-hamabe <1122hamabe.h@gmail.com>
2022-05-29 18:12:29 +0900

    swiper import & setting

--- js/index.js
+++ js/index.js
@@ -44,3 +44,25 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 500,
+  loop: true,
+  effect: "fade",
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
